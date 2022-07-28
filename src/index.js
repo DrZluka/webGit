c0655f362132c21789740dc8df3a766bed294b5b
Федор-ПК\Федор <curafedor@gmail.com>
2022-07-28 19:21:35 +0300

    init

--- src/index.js
+++ src/index.js
@@ -8,19 +8,20 @@ import webpackLogo from '@/images/webpack-logo.svg'
 import '@/styles/index.scss'
 
 // Appending to the DOM
-const logo = document.createElement('img')
-logo.src = webpackLogo
+//const logo = document.createElement('img')
+//logo.src = webpackLogo
 
-const heading = document.createElement('h1')
-heading.textContent = example()
-
-// Test a background image url in CSS
-const imageBackground = document.createElement('div')
-imageBackground.classList.add('image')
-
-// Test a public folder asset
-const imagePublic = document.createElement('img')
-imagePublic.src = '/assets/example.png'
-
-const app = document.querySelector('#root')
-app.append(logo, heading, imageBackground, imagePublic)
+// const heading = document.createElement('h1')
+// heading.textContent = example()
+//
+// // Test a background image url in CSS
+// const imageBackground = document.createElement('div')
+// imageBackground.classList.add('image')
+//
+// // Test a public folder asset
+// const imagePublic = document.createElement('img')
+// imagePublic.src = '/assets/example.png'
+//
+// const app = document.querySelector('#root')
+// app.append( heading, imageBackground, imagePublic)
+//тут стоял в app.append ещё и logo
